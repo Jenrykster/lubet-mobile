7599ab1d114e36747972f1ac672ee30f1b56e871
João Henrique <joaoenrique.san@gmail.com>
2022-03-10 15:47:35 -0300

    feat: integrate auth screens with the API

--- src/screens/AuthScreen/Forms/LoginForm/index.tsx
+++ src/screens/AuthScreen/Forms/LoginForm/index.tsx
@@ -1,35 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormContainer, Input, Label, Screen } from '../styles';
 import { TouchableText, Card } from '../../../../components/';
 import { ConfirmButton } from '../ConfirmButton';
 import { Title } from '../Title';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AuthNavigatorParamList } from '../../../../shared/types';
+import { login } from '../../../../shared/services';
+import { ActivityIndicator, Alert } from 'react-native';
+import { Colors } from '../../../../constants';
 
 type LoginProps = NativeStackScreenProps<AuthNavigatorParamList, 'Login'>;
 
 export const LoginForm = (props: LoginProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [typedEmail, setTypedEmail] = useState('');
+  const [typedPassword, setTypedPassword] = useState('');
+
+  const loginUser = async () => {
+    setIsLoading(true);
+
+    const result = await login(typedEmail, typedPassword);
+    if (result.status !== 200) {
+      Alert.alert('Error', result.data.message, [
+        { text: 'Ok', style: 'destructive' },
+      ]);
+    } else {
+      // Loga o usuário aqui
+      console.log(result.data.token);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <Screen>
       <Title />
       <FormContainer>
         <Card>
           <Label>Email</Label>
-          <Input keyboardType='email-address' placeholder='user@mail.com' />
+          <Input
+            autoCapitalize='none'
+            keyboardType='email-address'
+            placeholder='user@mail.com'
+            value={typedEmail}
+            onChangeText={setTypedEmail}
+          />
           <Label>Password</Label>
-          <Input secureTextEntry placeholder='secret' />
+          <Input
+            secureTextEntry
+            placeholder='secret'
+            value={typedPassword}
+            onChangeText={setTypedPassword}
+          />
           <TouchableText
             align='right'
             onPress={() => props.navigation.navigate('ResetPassword')}
           >
             I forgot my password
           </TouchableText>
-          <ConfirmButton
-            text='Log In'
-            primary
-            onPress={() => {}}
-            arrowDirection='FRONT'
-          />
+          {isLoading ? (
+            <ActivityIndicator size={60} color={Colors.primary} />
+          ) : (
+            <ConfirmButton
+              text='Log In'
+              primary
+              onPress={loginUser}
+              arrowDirection='FRONT'
+            />
+          )}
         </Card>
         <ConfirmButton
           arrowDirection='FRONT'
--- src/screens/AuthScreen/Forms/SignUpForm/index.tsx
+++ src/screens/AuthScreen/Forms/SignUpForm/index.tsx
@@ -1,29 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { FormContainer, Input, Label, Screen } from '../styles';
-import { TouchableText, Card } from '../../../../components/';
+import { Card } from '../../../../components/';
 import { ConfirmButton } from '../ConfirmButton';
 import { Title } from '../Title';
 import { AuthNavigatorParamList } from '../../../../shared/types';
+import { register } from '../../../../shared/services';
 
 type SignUpProps = NativeStackScreenProps<AuthNavigatorParamList, 'Login'>;
 
 export const SignUpForm = (props: SignUpProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [typedEmail, setTypedEmail] = useState('');
+  const [typedPassword, setTypedPassword] = useState('');
+  const [typedName, setTypedName] = useState('');
+
+  const registerUser = async () => {
+    setIsLoading(true);
+
+    const result = await register(typedName, typedEmail, typedPassword);
+    if (result.status === 400) {
+      Alert.alert('Error', result.data.error.message, [
+        { text: 'Ok', style: 'destructive' },
+      ]);
+    } else if (result.status !== 400 && result.status !== 200) {
+      Alert.alert('Error', 'Please use a valid e-mail', [
+        { text: 'Ok', style: 'destructive' },
+      ]);
+    } else {
+      // Loga o usuário aqui
+      console.log(result.data.token);
+    }
+
+    setIsLoading(false);
+  };
   return (
     <Screen>
       <Title />
       <FormContainer>
         <Card>
           <Label>Name</Label>
-          <Input placeholder='John Doe' />
+          <Input
+            placeholder='John Doe'
+            value={typedName}
+            onChangeText={setTypedName}
+          />
           <Label>Email</Label>
-          <Input keyboardType='email-address' placeholder='user@mail.com' />
+          <Input
+            autoCapitalize='none'
+            keyboardType='email-address'
+            placeholder='user@mail.com'
+            value={typedEmail}
+            onChangeText={setTypedEmail}
+          />
           <Label>Password</Label>
-          <Input secureTextEntry placeholder='secret' />
+          <Input
+            secureTextEntry
+            placeholder='secret'
+            value={typedPassword}
+            onChangeText={setTypedPassword}
+          />
           <ConfirmButton
             text='Register'
             primary
-            onPress={() => {}}
+            onPress={registerUser}
             arrowDirection='FRONT'
           />
         </Card>
