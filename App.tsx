07ef29ec8061862870e66b050112beb7da0c7f3e
João Henrique <joaoenrique.san@gmail.com>
2022-03-18 11:28:28 -0300

    feat: hide android navbar

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-
+import * as NavigationBar from 'expo-navigation-bar';
 import AppNavigator from './src/navigation/AppNavigator';
 import { store } from './src/store/store';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const hideNavbar = async () => {
+      setIsLoading(true);
+      await NavigationBar.setBehaviorAsync('overlay-swipe');
+      await NavigationBar.setVisibilityAsync('hidden');
+      setIsLoading(false);
+    };
+    hideNavbar();
+  }, []);
+  return <Provider store={store}>{!isLoading && <AppNavigator />}</Provider>;
 }
